53a138f7461299a57d7547e1dde183e7b7adeefa
John Regner <regnerjr@gmail.com>
2016-11-19 13:30:49 -0800

    Add initial VC to capture and send messages

---
+++ Connnect/SendViewController.swift
@@ -0,0 +1,46 @@
+import UIKit
+
+class SendViewController: UIViewController {
+
+    lazy var textDelegate:MessageTextDelegate = {
+        let del = MessageTextDelegate(msgHandler: { (message) in
+            let pub = AppDelegate.shared.client
+            pub?.publish(message, toChannel:"my_channel",
+                         compressed: false, withCompletion: nil)
+        })
+        return del
+    }()
+
+    @IBOutlet weak var messageTextField: UITextField! {
+        didSet {
+            self.messageTextField.delegate = self.textDelegate
+        }
+    }
+
+    override func viewDidLoad() {
+        navigationItem.title = "Send Message"
+
+    }
+
+}
+
+
+class MessageTextDelegate: NSObject, UITextFieldDelegate {
+
+    let callback: ((_ text:String) -> Void)
+
+    init(msgHandler: @escaping (String) -> Void) {
+        callback = msgHandler
+    }
+
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        guard let message = textField.text else { return false }
+        callback(message)
+        print("Send Message: \(message)")
+        textField.resignFirstResponder()
+
+        textField.text = ""
+        return true
+    }
+}
