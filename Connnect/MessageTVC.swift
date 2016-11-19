9b87960838c0a49853ac49c397ec159a067e04ae
yhu98 <yhu98@ucsc.edu>
2016-11-19 14:14:57 -0800

    fixed notificationcenter

--- Connnect/MessageTVC.swift
+++ Connnect/MessageTVC.swift
@@ -2,12 +2,12 @@ import UIKit
 import PubNub
 
 class MessageTVC: UITableViewController {
-    var appDelegate = UIApplication.shared.delegate as! AppDelegate
+    
     let notificationName = Notification.Name("NewMessage")
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        appDelegate.notification.addObserver(self, selector: #selector(MessageTVC.updateMessage), name: notificationName, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(MessageTVC.updateMessage), name: notificationName, object: nil)
 
     }
     
@@ -16,65 +16,21 @@ class MessageTVC: UITableViewController {
         tableView.reloadData()
     }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return appDelegate.myMessages.count
-    }
+    
+}
 
+
+extension MessageTVC {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return AppDelegate.shared.myMessages.count
+    }
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "messageCell", for: indexPath)
-
-        // Configure the cell...
-        cell.textLabel?.text = appDelegate.myMessages[indexPath.row]
-
+        
+        cell.textLabel?.text = AppDelegate.shared.myMessages[indexPath.row]
+        
         return cell
     }
-    
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
