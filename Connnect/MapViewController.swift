ca2cbd801cb3ec5d35b84e7e27fe2aeb918c6ec0
yhu98 <yhu98@ucsc.edu>
2016-11-20 13:35:33 -0800

    fixed always allow location issue

--- Connnect/MapViewController.swift
+++ Connnect/MapViewController.swift
@@ -26,7 +26,6 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
         MapViewState.hasBeenZoomed = false
         locationManager.delegate = AppDelegate.shared
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        locationManager.requestWhenInUseAuthorization()
         locationManager.requestAlwaysAuthorization()
         locationManager.startUpdatingLocation()
         mapView.showsUserLocation = true
@@ -123,12 +122,6 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
     
     
 }
-
-//extension MapViewController: MKMapViewDelegate {
-//    
-//
-//    
-//}
 
 extension MKCircle {
     var defaultRenderer: MKCircleRenderer {
